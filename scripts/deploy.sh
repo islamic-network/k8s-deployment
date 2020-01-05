0558494fdf78ca174bc34c9a6d6a98b0785284b0
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <meezaan@vesica.ws>
2020-01-05 22:39:11 +0400

    Check tags

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,14 +7,13 @@ COMMIT_TAG=$CIRCLE_TAG
 # We must export it so it's available for envsubst
 export COMMIT_TAG=$COMMIT_TAG
 
-echo $CIRCLE_TAG
-
-echo $COMMIT_TAG
 # since the only way for envsubst to work on files is using input/output redirection,
 #  it's not possible to do in-place substitution, so we need to save the output to another file
 #  and overwrite the original with that one.
 envsubst <./.kube/manifest.yml >./.kube/manifest.yml.out
 mv ./.kube/manifest.yml.out ./.kube/manifest.yml
+
+cat ./kube/manifest.yml
 
 echo "$K8S_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 
