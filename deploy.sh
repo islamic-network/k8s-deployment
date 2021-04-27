c1b7c29c2e69e24e292d32f05220872130581d68
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram / مِيزَانُ الْدِّينْ عَبْدُ ذِيْ الْجَلَالِ وَ الْإِكْرَامِ <m@meezaan.net>
2021-04-27 16:19:25 +0400

    Change to Asia

--- deploy.sh
+++ deploy.sh
@@ -13,11 +13,11 @@ export COMMIT_TAG=$COMMIT_TAG
 envsubst <./.k8s/manifest-europe.yml >./.k8s/manifest-europe.yml.out
 mv ./.k8s/manifest-europe.yml.out ./.k8s/manifest-europe.yml
 
-echo "$K8S_EUROPE_CERTIFICATE" | base64 --decode > cert.crt
+echo "$K8S_ASIA_CERTIFICATE" | base64 --decode > cert.crt
 
 ./kubectl \
   --kubeconfig=/dev/null \
-  --server=$K8S_EUROPE_SERVER \
+  --server=$K8S_ASIA_SERVER \
   --certificate-authority=cert.crt \
-  --token=$K8S_EUROPE_TOKEN \
+  --token=$K8S_ASIA_TOKEN \
   apply -f ./.k8s/
