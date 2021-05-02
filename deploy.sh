426418593d75b89f1fb812bfc7410bb766d708b3
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram / مِيزَانُ الْدِّينْ عَبْدُ ذِيْ الْجَلَالِ وَ الْإِكْرَامِ <m@meezaan.net>
2021-05-02 11:04:55 +0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,7 @@ export COMMIT_TAG=$COMMIT_TAG
 # since the only way for envsubst to work on files is using input/output redirection,
 #  it's not possible to do in-place substitution, so we need to save the output to another file
 #  and overwrite the original with that one.
+ls -la
 envsubst <./.k8s/manifest-europe.yml >./.k8s/manifest-europe.yml.out
 mv ./.k8s/manifest-europe.yml.out ./.k8s/manifest-europe.yml
 
