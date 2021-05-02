e3db4e2a80a74da90a66ef3b5404152019f3cace
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram / مِيزَانُ الْدِّينْ عَبْدُ ذِيْ الْجَلَالِ وَ الْإِكْرَامِ <m@meezaan.net>
2021-05-02 10:34:29 +0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,27 @@ COMMIT_TAG=$CIRCLE_TAG
 # We must export it so it's available for envsubst
 export COMMIT_TAG=$COMMIT_TAG
 
+
+
+"echo Deploying to the cluster in Europe..."
 # since the only way for envsubst to work on files is using input/output redirection,
 #  it's not possible to do in-place substitution, so we need to save the output to another file
 #  and overwrite the original with that one.
 envsubst <./.k8s/manifest-europe.yml >./.k8s/manifest-europe.yml.out
 mv ./.k8s/manifest-europe.yml.out ./.k8s/manifest-europe.yml
 
+echo "$K8S_EUROPE_CERTIFICATE" | base64 --decode > cert.crt
+
+./kubectl --kubeconfig=/dev/null --server=$K8S_EUROPE_SERVER --certificate-authority=cert.crt --token=$K8S_EUROPE_TOKEN apply -f ./.k8s/manifest-europe.yml
+
+"echo Deploying to the cluster in Asia..."
+# since the only way for envsubst to work on files is using input/output redirection,
+#  it's not possible to do in-place substitution, so we need to save the output to another file
+#  and overwrite the original with that one.
+envsubst <./.k8s/manifest-asia.yml >./.k8s/manifest-asia.yml.out
+mv ./.k8s/manifest-asia.yml.out ./.k8s/manifest-asia.yml
+
 echo "$K8S_ASIA_CERTIFICATE" | base64 --decode > cert.crt
 
-./kubectl --kubeconfig=/dev/null --server=$K8S_ASIA_SERVER --certificate-authority=cert.crt --token=$K8S_ASIA_TOKEN apply -f ./.k8s/
+./kubectl --kubeconfig=/dev/null --server=$K8S_ASIA_SERVER --certificate-authority=cert.crt --token=$K8S_ASIA_TOKEN apply -f ./.k8s/manifest-asia.yml
+
