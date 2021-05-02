beff039fb95b8fdae2867383d8502caa50c35346
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram / مِيزَانُ الْدِّينْ عَبْدُ ذِيْ الْجَلَالِ وَ الْإِكْرَامِ <m@meezaan.net>
2021-05-02 11:12:47 +0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,7 @@ COMMIT_TAG=$CIRCLE_TAG
 # We must export it so it's available for envsubst
 export COMMIT_TAG=$COMMIT_TAG
 
-"echo Deploying to the cluster in Europe..."
-echo $COMMIT_TAG
-ls -la
+echo "Deploying to the cluster in Europe..."
 envsubst <./.k8s/manifest-europe.yml >./.k8s/manifest-europe.yml.out
 mv ./.k8s/manifest-europe.yml.out ./.k8s/manifest-europe.yml
 
@@ -17,7 +15,7 @@ echo "$K8S_EUROPE_CERTIFICATE" | base64 --decode > cert.crt
 
 ./kubectl --kubeconfig=/dev/null --server=$K8S_EUROPE_SERVER --certificate-authority=cert.crt --token=$K8S_EUROPE_TOKEN apply -f ./.k8s/manifest-europe.yml
 
-"echo Deploying to the cluster in Asia..."
+echo "Deploying to the cluster in Asia..."
 # since the only way for envsubst to work on files is using input/output redirection,
 #  it's not possible to do in-place substitution, so we need to save the output to another file
 #  and overwrite the original with that one.
