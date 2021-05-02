61aced1391d3a3db06ff77cc1a42c9da64464b29
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2021-05-02 12:08:08 +0400

    Add separate deployments asia and europe

--- deploy-asia.sh
+++ deploy-asia.sh
@@ -7,14 +7,6 @@ COMMIT_TAG=$CIRCLE_TAG
 # We must export it so it's available for envsubst
 export COMMIT_TAG=$COMMIT_TAG
 
-echo "Deploying to the cluster in Europe..."
-envsubst <./.k8s/manifest-europe.yml >./.k8s/manifest-europe.yml.out
-mv ./.k8s/manifest-europe.yml.out ./.k8s/manifest-europe.yml
-
-echo "$K8S_EUROPE_CERTIFICATE" | base64 --decode > cert-eur.crt
-ls -la
-./kubectl apply -f ./.k8s/manifest-europe.yml --kubeconfig=/dev/null --server=$K8S_EUROPE_SERVER --certificate-authority=cert-eur.crt --token=$K8S_EUROPE_TOKEN
-
 echo "Deploying to the cluster in Asia..."
 envsubst <./.k8s/manifest-asia.yml >./.k8s/manifest-asia.yml.out
 mv ./.k8s/manifest-asia.yml.out ./.k8s/manifest-asia.yml
