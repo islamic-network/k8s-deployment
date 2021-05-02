5d28a333fa03a72a50ceb13131fd63fc03d8f5a6
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram / مِيزَانُ الْدِّينْ عَبْدُ ذِيْ الْجَلَالِ وَ الْإِكْرَامِ <m@meezaan.net>
2021-05-02 11:53:56 +0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ mv ./.k8s/manifest-europe.yml.out ./.k8s/manifest-europe.yml
 
 echo "$K8S_EUROPE_CERTIFICATE" | base64 --decode > cert-eur.crt
 ls -la
-./kubectl --kubeconfig=/dev/null --server=$K8S_EUROPE_SERVER --certificate-authority=cert-eur.crt --token=$K8S_EUROPE_TOKEN apply -f ./.k8s/manifest-europe.yml
+./kubectl apply -f ./.k8s/manifest-europe.yml --kubeconfig=/dev/null --server=$K8S_EUROPE_SERVER --certificate-authority=cert-eur.crt --token=$K8S_EUROPE_TOKEN
 
 echo "Deploying to the cluster in Asia..."
 envsubst <./.k8s/manifest-asia.yml >./.k8s/manifest-asia.yml.out
@@ -21,5 +21,5 @@ mv ./.k8s/manifest-asia.yml.out ./.k8s/manifest-asia.yml
 
 echo "$K8S_ASIA_CERTIFICATE" | base64 --decode > cert-asia.crt
 
-./kubectl --kubeconfig=/dev/null --server=$K8S_ASIA_SERVER --certificate-authority=cert-asia.crt --token=$K8S_ASIA_TOKEN apply -f ./.k8s/manifest-asia.yml
+./kubectl apply -f ./.k8s/manifest-asia.yml --kubeconfig=/dev/null --server=$K8S_ASIA_SERVER --certificate-authority=cert-asia.crt --token=$K8S_ASIA_TOKEN
 
