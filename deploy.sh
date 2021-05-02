c8a41ed99221355840fce5aaa6c80b9d0012eb41
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram / مِيزَانُ الْدِّينْ عَبْدُ ذِيْ الْجَلَالِ وَ الْإِكْرَامِ <m@meezaan.net>
2021-05-02 11:40:50 +0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ echo "Deploying to the cluster in Europe..."
 envsubst <./.k8s/manifest-europe.yml >./.k8s/manifest-europe.yml.out
 mv ./.k8s/manifest-europe.yml.out ./.k8s/manifest-europe.yml
 
-echo "$K8S_EUROPE_CERTIFICATE" > cert.crt
+echo "$K8S_EUROPE_CERTIFICATE" | base64 --decide > cert.crt
 ls -la
 ./kubectl --kubeconfig=/dev/null --server=$K8S_EUROPE_SERVER --certificate-authority=cert.crt --token=$K8S_EUROPE_TOKEN apply -f ./.k8s/manifest-europe.yml
 
